08e4be9f616aa288ac451c2540cb0a4b23f6295f
eliasjr <elias.dvlpr@gmail.com>
2019-11-24 11:18:25 -0400

    components e bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjetoNovoComponent } from './projeto-novo/projeto-novo.component';
+import { ProjetoListaComponent } from './projeto-lista/projeto-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetoNovoComponent,
+    ProjetoListaComponent
   ],
   imports: [
     BrowserModule,
